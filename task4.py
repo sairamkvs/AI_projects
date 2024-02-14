fadd62dc5e66dc2e858f294f112bb1df935f8a20
SaiRam <118681989+sairamkvs@users.noreply.github.com>
2024-02-14 20:45:55 +0530

    task4.py

---
+++ task4.py
@@ -0,0 +1,15 @@
+# Define two sets
+E = {0, 2, 4, 6, 8}
+N = {1, 2, 3, 4, 5}
+
+# Union of sets
+print("Union of E and N is",E | N)
+
+# Intersection of sets
+print("Intersection of E and N is",E & N)
+
+# Difference of two sets
+print("Difference of E and N is",E - N)
+
+# Symmetric difference of two sets
+print("Symmetric difference of E and N is",E ^ N)
